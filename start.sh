9e606a1945f59f26bd4665911ac715a270b8c0a3
alonbaby <82175004+alonbaby@users.noreply.github.com>
2021-04-09 01:54:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DEJ95C287o9e7vRP3cXZA9xZT7dmP1ZUqB -p c=DOGE
 done
